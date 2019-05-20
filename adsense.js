dc34443c9cde1b9a94595f920222e33f7c1c8bbf
ugob24 <50824663+ugob24@users.noreply.github.com>
2019-05-20 09:51:20 +0100

    Add files via upload

---
+++ adsense.js
@@ -0,0 +1,11 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 9japarlour_header2_AdSense1_1x1_as -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9194128235235649"
+     data-ad-slot="8839419721"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
